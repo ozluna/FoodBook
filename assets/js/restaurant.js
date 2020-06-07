601bcf277224c2923351cadb1915de235a4733ae
Ozge <ozonel883@hotmail.com>
2020-06-07 09:58:32 +0000

    trying to use tripadvisor api on my website

--- assets/js/restaurant.js
+++ assets/js/restaurant.js
@@ -1,26 +1,19 @@
-var unirest = require("unirest");
+var data = null;
 
-var req = unirest("GET", "https://tripadvisor1.p.rapidapi.com/restaurants/list-by-latlng");
-
-req.query({
-	"limit": "30",
-	"currency": "USD",
-	"distance": "2",
-	"lunit": "km",
-	"lang": "en_US",
-	"latitude": "12.91285",
-	"longitude": "100.87808"
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+function setData(jsonData){
+    data = jsonData;
+    console.log(data);
+}
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		setData(JSON.parse(this.responseText));
+	}
 });
 
-req.headers({
-	"x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
-	"x-rapidapi-key": "d9b6260d09mshf173d56e7c8cb49p1dfbc7jsn19ee9d462231",
-	"useQueryString": true
-});
+xhr.open("GET", "https://tripadvisor1.p.rapidapi.com/restaurants/list-by-latlng?limit=30&currency=USD&distance=2&lunit=km&lang=en_US&latitude=12.91285&longitude=100.87808");
+xhr.setRequestHeader("x-rapidapi-host", "tripadvisor1.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "d9b6260d09mshf173d56e7c8cb49p1dfbc7jsn19ee9d462231");
 
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body);
-});
+xhr.send(data);
