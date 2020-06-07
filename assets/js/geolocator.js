364dc4a0f411e9e3cd7bbc2de60906a77d5b7e70
Ozge <ozonel883@hotmail.com>
2020-06-07 09:57:33 +0000

    trying to understand how the api works

--- assets/js/geolocator.js
+++ assets/js/geolocator.js
@@ -1,15 +1,40 @@
-var postCode = $("#post-code").val();
-if(!postCode){
-    $("#result").html(`<h2>Please enter a valid post code.</h2>`);
-    return;
+  var x = document.getElementById("demo");
+  function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+    var userLat
+    var userLng
+    function showPosition(position) {
+        document.getElementById("demo").innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+        userLat =parseInt(position.coords.latitude,10);
+        userLng =parseInt(position.coords.longitude,10);
+        console.log(userLat+"  "+ userLng);
+    }
+    
+
+    function initMap() {
+    var nearme = { lat:52.205337,lng:0.12181699999996454}
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center:nearme
+           
+        
+    });
+   
+    
+   
+function addMarker(coords){
+    var marker = new google.maps.Marker({
+        position: coords,
+        map:map,
+        icon: "http://maps.gstatic.com/mapfiles/place_api/icons/restaurant-70.png" 
+     });
+    
 }
-$("#result").html(`<div id=loader>
-<img src="assets/image/22.gif" alt="loading.."/></div>`)
-
-function getlocation(postCode){
-    console.log(postCode);
-    return `<p>${postCode}</p>`;
-
+addMarker({ lat:52.205337,lng:0.12181699999996454})
 }
-
-
