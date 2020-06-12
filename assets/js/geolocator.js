9c18b01b9fd96504eed33bc1f448f47f586e19a4
Ozge <ozonel883@hotmail.com>
2020-06-12 13:46:44 +0000

    error code added incase of block location

--- assets/js/geolocator.js
+++ assets/js/geolocator.js
@@ -11,8 +11,7 @@ function initMap() {
 
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            
+          navigator.geolocation.getCurrentPosition(function(position) {            
             var pos = {
               lat: position.coords.latitude,
               lng: position.coords.longitude
@@ -26,25 +25,36 @@ function initMap() {
             infoWindow.open(map);
             map.setCenter(pos);
           }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
+            handleLocationError(true, infoWindow);
           });
         } else {
           // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
+          handleLocationError(false, infoWindow);
         }
-        // adding nearby restaurants I will use places google        
+             
      
     
 }
-    var geoSuccess = function(position) {
-    startPos = position;
-    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-  };
-  navigator.geolocation.getCurrentPosition(geoSuccess);    
-  
+    // Handle a geolocation error
+    function handleLocationError(browserHasGeolocation, infoWindow) {
+        // Set default location to Sydney, Australia
+        pos = {lat:52.2141579, lng: 0.147892};
+        map = new google.maps.Map(document.getElementById('map'), {
+        center: pos,
+        zoom: 15
+        });
 
+        // Display an InfoWindow at the map center
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+        'Geolocation permissions denied. Using default location.' :
+        'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+        currentInfoWindow = infoWindow;
 
+    /* TODO: Step 3B3, Call the Places Nearby Search */
+}
+// adding nearby restaurants I will use places google   
 //adding markers for the restaurants
 //set an radius 
 //add event listener for the buttons restaurants , cafes , tourist attractions and hotels
